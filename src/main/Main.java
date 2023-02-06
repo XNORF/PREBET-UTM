7597b700f933faa61bd04bec77c19ff3282b0949
Amirul Irfan <amirulirfan.utm@gmail.com>
2023-02-06 16:01:23 +0800

    Did UI Designs for 5 pages.

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
